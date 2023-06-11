ed5d77889462109511ed0f6053b3e43d0ad95800
Amin <1persian.gulf@gmail.com>
2023-06-11 10:49:30 +0330

    2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+
   title = 'SimpleValidator';
+  myForm! :FormGroup;
+  firstName='';
+
+
+  ngOnInit(): void {
+    this.myForm = new FormGroup({
+      name: new FormControl([
+        Validators.required
+      ])
+    });
+  }
+
+  submitForm() {
+    console.log('form submited!');
+    if (this.myForm.valid) {
+      this.callMyAPI();
+    }
+    else {
+    }
+  }
+
+  txtChange(){
+    console.log('textbox changed!');
+    if(this.firstName.length>4){
+      this.callMyAPI();
+    }
+  }
+
+  callMyAPI() {
+    window.alert('سرویس اجرا شد!');
+  }
+
 }
